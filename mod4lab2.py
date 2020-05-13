ac3c5ce4856573fcb43c7a43e82e6486bb65c915
Eddy Bramel <e.lamanna-bramel@students.clark.edu>
2020-05-13 13:39:29 -0700

    following along in class

--- mod4lab2.py
+++ mod4lab2.py
@@ -12,8 +12,33 @@ Output: return value and description
 """
 
 
+
+
+from graphics import *
 def main():
-    # code here
+    '''
+    startValue = 1
+    endValue = 10
+    sum = 0
+    if startValue % 2 == 1:
+        startValue = startValue + 1
+    for i in range(startValue, endValue, 2):
+        sum = sum + i
+    print(sum)
+    '''
+    # example of setting own coordinates
+    win = GraphWin("demo", 800, 800)
+    win.setCoords(-4.0, -4.0, 4.0, 4.0)
+    p1 = Circle(Point(2, 3), 1)
+    p1.setFill("red")
+    p1.draw(win)
+    p2 = Circle(Point(-3, 1), 1)
+    p2.setFill("black")
+    p2.draw(win)
+    p3 = Circle(Point(-1.5, -2.5), 1)
+    p3.setFill("blue")
+    p3.draw(win)
+    input()
 
 
 main()
